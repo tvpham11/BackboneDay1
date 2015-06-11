8bb7b33f0e68fca49a1b63b3f2945b5724a9403e
Vi Pham <tvpham11@gmail.com>
2015-06-11 12:10:22 -0400

    added styling and student.collection.js

---
+++ app/js/collections/student.collection.js
@@ -0,0 +1,7 @@
+var StudentCollection = Backbone.Collection.extend({
+
+  model: Student,
+
+  url: 'http://tiy-515.herokuapp.com/collections/alkasjflksadfkldsajlkfjklasdf',
+
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,46 @@
-var students = ['Justin', 'James', 'Jack', 'Max'];
+/*var students = ['Justin', 'James', 'Jack', 'Max'];
+
+var allStudents = new StudentCollection();
 
 students.forEach(function(studentName) {
-  new Student({
+
+  var s = new Student({
     name: studentName
   });
-});
 
-// var x = new Student({
-//   name: 'Vi',
-//   location: 'Atlanta',
-//   favorite_food: 'Mangos'
-// });
+  allStudents.add(s);
+
+});*/
+
+var allStudents = new StudentCollection();
+
+var addStudent = function(e) {
+  e.preventDefault();
+
+  var studentName = $(this).find('input').val();
+
+  var s = new Student({
+    name: studentName
+  });
+
+  allStudents.add(s).save().success(function(data) {
+    console.log(data);
+    addStudentToView(data);
+  });
+
+  this.reset(); // Reset form
+
+};
+
+var addStudentToView = function(student) {
+  var studentHtml = "<li id='" + student._id + "'>";
+      studentHtml += student.name;
+      studentHtml += "</li>";
+
+  $('#students').prepend(studentHtml);
+};
+
+
+// From Submit Listener
+$('#addStudent').on('submit', addStudent);
+
